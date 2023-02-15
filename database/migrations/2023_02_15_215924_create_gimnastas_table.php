d06d810bd2f5bf5f0cb8f12f6aa11d8de884508a
Jesus Perez Hernandez <yizussawyer@gmail.com>
2023-02-15 16:53:11 -0600

    Modelos, controladores y migraciones

---
+++ database/migrations/2023_02_15_215924_create_gimnastas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGimnastasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() //avanza el diseño
+    {
+        Schema::create('gimnastas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_g');
+            $table->string('apellido_g');
+            $table->date('fecha_n_g');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() //ctrl z
+    {
+        Schema::dropIfExists('gimnastas');
+    }
+}
